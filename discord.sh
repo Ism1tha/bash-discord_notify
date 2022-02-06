7241df21a3bd1d38d6c993bf0d98895470454f7d
Ismael Semmar Galvez <30930942+Ism1tha@users.noreply.github.com>
2022-02-06 13:29:00 +0100

    Update discord.sh

--- discord.sh
+++ discord.sh
@@ -1,10 +1,10 @@
 #!/bin/sh  
 while true  
 do 
- title='\n**Ism1tha cr4cking server: Hashcat v5.1.0**\n\n' 
+ title='\n**Message title**\n\n' 
  msg_content=\"${title}$(tail nohup.out | grep Rec)\\n$(tail nohup.out | grep Prog)\"
  echo $msg_content
- url='https://discordapp.com/api/webhooks/938552332688429096/umUiCym48SCHotGGTwLWn6npqeciL0Z3DWqNjOygkNiulRgJuxDap9XNg75i8DNaIuPw'
+ url='PASTE_YOUR_WEBHOOK_HERE'
  curl -H "Content-Type: application/json" -X POST -d "{\"content\": $msg_content}" $url  
   sleep 7200  
 done
